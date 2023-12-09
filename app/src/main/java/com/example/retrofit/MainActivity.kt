b148bc064f9cf64315b44f7cd0e5d3e5d39e3c1a
srujan_regulapati <sregulapati4@gmail.com>
2023-12-09 06:25:45 +0530

    donee

---
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.retrofit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.retrofit.ui.theme.RetrofitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RetrofitTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RetrofitTheme {
+        Greeting("Android")
+    }
+}
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+/**
+ * Build the Moshi object with Kotlin adapter factory that Retrofit will be using.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * The Retrofit object with the Moshi converter.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * A public interface that exposes the [getPhotos] method
+ */
+interface MarsApiService {
+    /**
+     * Returns a [List] of [MarsPhoto] and this method can be called from a Coroutine.
+     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
+     * HTTP method
+     */
+    @GET("photos")
+    suspend fun getPhotos() : List<MarsPhoto>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object MarsApi {
+    val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
+}
